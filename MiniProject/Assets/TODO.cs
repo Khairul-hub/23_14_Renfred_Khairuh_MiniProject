c5ee6116b7960b79b13c8a0943fc3fd7d7ff4622
pepperboxs <58922915+pepperboxs@users.noreply.github.com>
2020-08-19 13:44:17 +0800

    Test

--- MiniProject/Assets/TODO.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
- * Create a function on each shape tp assign adjacent shapes to GameObject[] variable ( For Checking if Swappable )
- * use RayCast or Collision to get adjacent Shapes ( Unless u got better method ) 
- * Run the Function only at Game Start ( After Shapes Fall ) and Right Afte Swapping
- *  
- *  
- * Use Adjacent Blocks to check if they're any viable moves left 
-*/
-
